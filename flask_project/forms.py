9274759b1ff4c58ac4ececab15670cf6bd387374
HajraRafiq <hajrarafiq1828@gmail.com>
2022-10-12 20:38:27 +0500

    Form Validation

--- flask_project/forms.py
+++ flask_project/forms.py
@@ -1,5 +1,3 @@
-
-
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
@@ -12,7 +10,6 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
-
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
